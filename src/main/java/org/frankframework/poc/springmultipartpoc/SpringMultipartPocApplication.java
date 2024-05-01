cb84936bb2d6aea661a813189b7bd91f49d8ffe6
Matthbo <4380412+Matthbo@users.noreply.github.com>
2024-05-01 13:39:10 +0200

    Initial commit

---
+++ src/main/java/org/frankframework/poc/springmultipartpoc/SpringMultipartPocApplication.java
@@ -0,0 +1,13 @@
+package org.frankframework.poc.springmultipartpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultipartPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultipartPocApplication.class, args);
+    }
+
+}
